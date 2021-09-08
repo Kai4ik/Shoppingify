ffcfa6f2dbf22333c33191f0ff4c0a937ea1ea0f
Kairat Orozobekov <orozobekov.kai@gmail.com>
2021-09-08 19:52:52 -0400

    DB configuration changed

--- models/database.js
+++ models/database.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-
 const Sequelize = require("sequelize");
-const db = new Sequelize("shoppingify", "postgres", `qwAS12==66`, {
+
+const db = new Sequelize(process.env.DATABASE_URL, {
   host: "localhost",
   dialect: "postgres",
+  port: 5432,
+  logging: true,
 });
 
 module.exports = db;
